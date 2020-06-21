9debc4d4eab40790f1bb46a547999c2078d984d9
Jyoti <jyoti.contact@gmail.com>
2020-06-21 22:12:07 +0100

    create reusable ArticlesList component

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import ArticlesList from '../components/ArticlesList';
+import articleContent from './article-content';
 
-const ArticlePage = () => (
-  <>
-    <h1>This is an Article</h1>
-  </>
-);
+const ArticlePage = ({ match }) => {
+  const name = match.params.name;
+  const article = articleContent.find((article) => article.name === name);
 
+  if (!article) return <h1>Sorry, no article found!</h1>;
+
+  const otherArticles = articleContent.filter(
+    (article) => article.name !== name
+  );
+
+  return (
+    <>
+      <h1>{article.title}</h1>
+      {article.content.map((paragraph, key) => (
+        <p key={key}>{paragraph}</p>
+      ))}
+      <h2>Other Articles:</h2>
+      <ArticlesList articles={otherArticles} />
+    </>
+  );
+};
 export default ArticlePage;
