539a21fc1c6651903fdbc542f781d0e5d555f172
Jyoti <jyoti.contact@gmail.com>
2020-06-21 21:03:45 +0100

    set up React Router

---
+++ src/pages/HomePage.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const HomePage = () => (
+  <>
+    <h1>Hello, welcome to my blog!</h1>
+    <p>
+      Welcome to my blog! Lorem ipsum dolor sit amet, consectetur adipiscing
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Quam pellentesque nec nam aliquam sem et. Nunc lobortis mattis aliquam
+      faucibus purus in massa tempor. Adipiscing elit duis tristique
+      sollicitudin nibh sit amet commodo. Varius duis at consectetur lorem
+      donec. Rhoncus aenean vel elit scelerisque mauris. Tincidunt lobortis
+      feugiat vivamus at augue eget. Aliquet eget sit amet tellus cras
+      adipiscing. Duis at tellus at urna condimentum mattis pellentesque. Odio
+      facilisis mauris sit amet massa vitae tortor. Aliquam sem et tortor
+      consequat id porta nibh venenatis cras. Fermentum et sollicitudin ac orci
+      phasellus. Lectus proin nibh nisl condimentum id.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Quam pellentesque nec
+      nam aliquam sem et. Nunc lobortis mattis aliquam faucibus purus in massa
+      tempor. Adipiscing elit duis tristique sollicitudin nibh sit amet commodo.
+      Varius duis at consectetur lorem donec. Duis at tellus at urna condimentum
+      mattis pellentesque. Odio facilisis mauris sit amet massa vitae tortor.
+      Aliquam sem et tortor consequat id porta nibh venenatis cras. Fermentum et
+      sollicitudin ac orci phasellus. Lectus proin nibh nisl condimentum id.
+    </p>
+  </>
+);
+
+export default HomePage;
