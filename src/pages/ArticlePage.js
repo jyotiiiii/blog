d3cd0673f85765086c5713f2266c93f406154131
Jyoti <jyoti.contact@gmail.com>
2020-06-26 20:17:54 +0100

    connect to api

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
+import CommentsList from '../components/CommentsList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
 
@@ -7,7 +8,19 @@ const ArticlePage = ({ match }) => {
   const name = match.params.name;
   const article = articleContent.find((article) => article.name === name);
 
-    if (!article) return <NotFoundPage />;
+  const [articleInfo, setArticleInfo] = useState({ upvotes: 0, comments: [] });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(`/api/articles/${name}`);
+      const body = await result.json();
+      console.log({ body });
+      setArticleInfo(body);
+    };
+    fetchData();
+  }, [name]);
+
+  if (!article) return <NotFoundPage />;
 
   const otherArticles = articleContent.filter(
     (article) => article.name !== name
@@ -16,9 +29,11 @@ const ArticlePage = ({ match }) => {
   return (
     <>
       <h1>{article.title}</h1>
+      <p>This post has been upvoted {articleInfo.upvotes} times</p>
       {article.content.map((paragraph, key) => (
         <p key={key}>{paragraph}</p>
       ))}
+      <CommentsList comments={articleInfo.comments} />
       <h2>Other Articles:</h2>
       <ArticlesList articles={otherArticles} />
     </>
